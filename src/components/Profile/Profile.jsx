92374de83c19e3b85d3f09edb229466b531452d3
kir-dev13 <kirill87rabota@gmail.com>
2021-05-29 18:08:46 +0300

    init

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <div>
+                <img
+                    src="https://atlantis.nyc3.digitaloceanspaces.com/media/legacy/atlantis/Things_To_Do/Water_Park/Beaches/Hero/Experiences_Beach.jpg"
+                    alt=""
+                />
+            </div>
+            <div>ava + descr</div>
+            <div>
+                my post
+                <div>new post</div>
+            </div>
+            <div>Postst</div>
+        </main>
+    );
+};
+
+export default Profile;
